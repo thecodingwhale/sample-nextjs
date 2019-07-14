654f280f428aaf98a668dd87166249c9528868e6
Aldren Terante <aldren.terante@gmail.com>
2019-07-14 20:15:11 +0800

    update about page

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 const path = require('path')
+const withImages = require('next-images')
 
-module.exports = {
+module.exports = withImages({
   webpack (config, options) {
     config.resolve.alias['components'] = path.join(__dirname, 'src/components')
     config.resolve.alias['assets'] = path.join(__dirname, 'src/assets')
     config.resolve.alias['variables'] = path.join(__dirname, 'src/variables')
     return config
   }
-}
+})
